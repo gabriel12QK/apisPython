0de61cd2889d070ad91573b14faf68d100898e26
gabriel12QK <jose.rivas@espam.edu.ec>
2022-10-25 16:10:54 -0500

    make it better

--- proyecto_API/proyecto_API/settings.py
+++ proyecto_API/proyecto_API/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qa=eqd&68szk+9sgjh66vk8w*12k92b8s=bcscq(ikxmx@-78t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
